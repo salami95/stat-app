c2ec7be91b499c0552fd5a2648eafe1d609cdef8
Kevin Salimi <60047936+salami95@users.noreply.github.com>
2025-04-07 13:47:59 -0400

    Update podcast_script_generator.py

--- podcast_script_generator.py
+++ podcast_script_generator.py
@@ -2,7 +2,7 @@
 
 import os
 from langchain_community.chat_models import ChatOpenAI
-from langchain_community.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate
 
 
 # Generate a script for a single topic using student context + grounded facts
