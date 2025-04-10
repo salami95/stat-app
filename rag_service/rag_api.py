7bb1353ec2d835789a82437cd904dc388c73740a
Kevin Salimi <60047936+salami95@users.noreply.github.com>
2025-04-10 19:10:34 -0400

    Update rag_api.py

--- rag_service/rag_api.py
+++ rag_service/rag_api.py
@@ -4,6 +4,12 @@ import os
 
 app = Flask(__name__)
 
+# URL for external communication (if needed)
+WHISPER_URL = os.getenv("WHISPER_URL", "http://whisperservice-production.up.railway.app:80")
+TOPIC_URL = os.getenv("TOPIC_URL", "http://topicservice-production.up.railway.app:80")
+RAG_URL = os.getenv("RAG_URL", "http://ragservice-production.up.railway.app:80")
+SCRIPTGEN_URL = os.getenv("SCRIPTGEN_URL", "http://scriptgenservice-production.up.railway.app:80")
+
 def load_medrag_vectorstore():
     persist_dir = os.path.join(os.path.dirname(__file__), "../rag/medrag_index")
     storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
